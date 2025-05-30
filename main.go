086f16b72e8042d7c011d79d6258200442b79a9f
mf751 <mf751f@gmail.com>
2025-05-30 09:09:43 +0300

    simple testing

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"context"
 	"embed"
 
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+
+	"pharmaB/backend"
 )
 
 //go:embed all:frontend/dist
@@ -13,20 +16,23 @@ var assets embed.FS
 
 func main() {
 	// Create an instance of the app structure
-	app := NewApp()
+	app := backend.NewApp()
 
 	// Create application with options
 	err := wails.Run(&options.App{
 		Title:  "pharmaB",
-		Width:  1024,
-		Height: 768,
+		Width:  1920,
+		Height: 1024,
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
-		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
-		OnStartup:        app.startup,
+		BackgroundColour: &options.RGBA{R: 229, G: 229, B: 229, A: 1},
+		OnStartup:        app.Startup,
 		Bind: []interface{}{
 			app,
+		},
+		OnShutdown: func(ctx context.Context) {
+			app.DataModel.DB.Close()
 		},
 	})
 	if err != nil {
