ed39dd0206674d7696bbb980eab2ad23065831fa
mf751 <mf751f@gmail.com>
2025-05-30 09:09:16 +0300

    simple testint

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,50 @@
-import "./App.css";
-import { Greet } from "../wailsjs/go/main/App";
+import { useState, useRef } from "react";
+import { AddTest, GetTests } from "../wailsjs/go/backend/App";
 
 function App() {
-  // function greet() {
-  // Greet(name).then(updateResultText);
-  // }
+  const [err, setErr] = useState(false);
+  const [tests, setTests] = useState([]);
+  const txtRef = useRef(null);
+  const [msg, setMsg] = useState("");
 
-  return <div id="App"></div>;
+  const addTest = () => {
+    AddTest(txtRef.current.value).then((errored) => setErr(errored));
+  };
+
+  return (
+    <div id="App">
+      <br />
+      <input type="text" ref={txtRef} />
+      <br />
+      <button onClick={addTest}>Add test</button>
+      <br />
+      <div>Errored: {err ? "added" : "failed"}</div>
+      <br />
+      <br />
+      <button
+        onClick={() =>
+          GetTests().then((tests) => {
+            setTests(tests);
+            setMsg(tests);
+          })
+        }
+      >
+        Get Tests
+      </button>
+      <div>
+        Tests:{" "}
+        {tests.map((test, idx) => (
+          <div key={idx}>
+            {idx}: {test}
+          </div>
+        ))}
+      </div>
+      <br />
+      <br />
+      <br />
+      <div>Message {msg}</div>
+    </div>
+  );
 }
 
 export default App;
